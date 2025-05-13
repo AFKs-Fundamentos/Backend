1761f437c7d24554c60bf70f6e0dffa064eb733c
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-05-13 03:59:34 -0500

    feat(advisory-management): add basic configs for development environment

---
+++ src/main/java/com/pcmaster/AFK/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.pcmaster.AFK.config; // <--- Ajusta según tu estructura
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .csrf(CsrfConfigurer::disable); // ← nueva forma funcional de desactivar CSRF
+
+        return http.build();
+    }
+}
