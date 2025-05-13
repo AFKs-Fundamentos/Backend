31bd9ae045a073e40e8308ae4e4de3541fbb0d0b
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-05-13 04:00:03 -0500

    feat(advisory-management): add transform resources

---
+++ src/main/java/com/pcmaster/AFK/advisorymanagement/interfaces/rest/transform/AdvisoryResourceFromEntityToAssembler.java
@@ -0,0 +1,22 @@
+package com.pcmaster.AFK.advisorymanagement.interfaces.rest.transform;
+
+import com.pcmaster.AFK.advisorymanagement.domain.model.aggregates.Advisory;
+import com.pcmaster.AFK.advisorymanagement.interfaces.rest.resources.AdvisoryResource;
+
+public class AdvisoryResourceFromEntityToAssembler {
+    public static AdvisoryResource toResourceFromEntity(Advisory advisory) {
+        return new AdvisoryResource(
+                advisory.getId(),
+                advisory.getAdvisoryType(),
+                advisory.getAdvisoryStatus(),
+                advisory.getAdvisorId().advisorId(),
+                advisory.getCustomerId().customerId(),
+                advisory.getAdvisoryDate(),
+                advisory.getAdvisoryTime(),
+                advisory.getMeetUrl(),
+                advisory.getClientEmail(),
+                advisory.getAdvisoryDescription(),
+                advisory.getLocation()
+        );
+    }
+}
