98d49d851cb0d3a786ed2cedeca6c307cfee25f7
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-05-13 03:55:03 -0500

    feat: add value objects

---
+++ src/main/java/com/pcmaster/AFK/advisorymanagement/domain/model/valueobjects/AdvisoryStatus.java
@@ -0,0 +1,8 @@
+package com.pcmaster.AFK.advisorymanagement.domain.model.valueobjects;
+
+public enum AdvisoryStatus {
+    PENDING,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED,
+}
---
+++ src/main/java/com/pcmaster/AFK/advisorymanagement/domain/model/valueobjects/AdvisoryType.java
@@ -0,0 +1,7 @@
+package com.pcmaster.AFK.advisorymanagement.domain.model.valueobjects;
+
+public enum AdvisoryType {
+    VIRTUAL,
+    IN_PERSON,
+}
+
