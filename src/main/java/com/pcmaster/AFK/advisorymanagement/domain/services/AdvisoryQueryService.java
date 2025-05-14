ce51e15f04b8c59ce5ea375f1101bf82a3670ec7
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-05-14 01:45:51 -0500

    feat(advisory-management): add get advisory type query in query service

--- src/main/java/com/pcmaster/AFK/advisorymanagement/domain/services/AdvisoryQueryService.java
+++ src/main/java/com/pcmaster/AFK/advisorymanagement/domain/services/AdvisoryQueryService.java
@@ -1,10 +1,7 @@
 package com.pcmaster.AFK.advisorymanagement.domain.services;
 
 import com.pcmaster.AFK.advisorymanagement.domain.model.aggregates.Advisory;
-import com.pcmaster.AFK.advisorymanagement.domain.model.queries.GetAdvisoriesByAdvisorIdQuery;
-import com.pcmaster.AFK.advisorymanagement.domain.model.queries.GetAdvisoriesByAdvisoryStatusQuery;
-import com.pcmaster.AFK.advisorymanagement.domain.model.queries.GetAdvisoriesByCustomerIdQuery;
-import com.pcmaster.AFK.advisorymanagement.domain.model.queries.GetAdvisoryByIdQuery;
+import com.pcmaster.AFK.advisorymanagement.domain.model.queries.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,8 +9,10 @@ import java.util.Optional;
 public interface AdvisoryQueryService {
 
     List<Advisory> handle(GetAdvisoriesByAdvisoryStatusQuery query);
+    List<Advisory> handle(GetAdvisoriesByAdvisoryTypeQuery query);
     List<Advisory> handle(GetAdvisoriesByCustomerIdQuery query);
     List<Advisory> handle(GetAdvisoriesByAdvisorIdQuery query);
+
 
     Optional<Advisory> handle(GetAdvisoryByIdQuery query);
 }
