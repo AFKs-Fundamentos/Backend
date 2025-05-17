f2fc6ac911c30ad7f71f030979f13d5d58ee96c7
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-05-17 02:31:36 -0500

    feat: add repositorie product.

---
+++ src/main/java/com/pcmaster/AFK/product_managment/infrastructure/persistence/jpa/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.pcmaster.AFK.product_managment.infrastructure.persistence.jpa.repositories;
+
+import com.pcmaster.AFK.product_managment.domain.model.aggregates.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
