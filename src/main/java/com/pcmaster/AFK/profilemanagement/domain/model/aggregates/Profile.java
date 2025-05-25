da1daedfd2b7724480fa3b8daa441c2b8474a937
David Alejandro Rivas Sarango <u20191e831@upc.edu.pe>
2025-05-25 12:58:16 -0500

    feat(profile-management): added base Profile entity

---
+++ src/main/java/com/pcmaster/AFK/profilemanagement/domain/model/aggregates/Profile.java
@@ -0,0 +1,53 @@
+package com.pcmaster.AFK.profilemanagement.domain.model.aggregates;
+
+
+import com.pcmaster.AFK.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "profiles")
+@Setter
+@Getter
+@NoArgsConstructor
+public class Profile extends AuditableAbstractAggregateRoot<Profile> {
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    @Email(message = "El email registrado no es válido.")
+    private String email;
+    @Column(name = "phone")
+    private String phone;
+
+    public Profile(String firstName, String lastName, String email, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Profile(CreateProfileCommand command){
+        this.firstName = command.getFirstName();
+        this.lastName = command.getLastName();
+        this.email = command.getEmail();
+        this.phone = command.getPhone();
+    }
+
+    public Profile updateProfile(String firstName, String lastName, String email, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        return this;
+    }
+
+
+}
