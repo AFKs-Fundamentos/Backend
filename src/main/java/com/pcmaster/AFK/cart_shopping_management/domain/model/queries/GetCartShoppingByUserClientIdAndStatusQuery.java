176ca98d9ebb7e9983bcd3f58f8efd44dec3b67f
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-06-01 18:03:48 -0500

    feat(cart-shopping): add in queries create  record get cart shopping by user client id and status query.

---
+++ src/main/java/com/pcmaster/AFK/cart_shopping_management/domain/model/queries/GetCartShoppingByUserClientIdAndStatusQuery.java
@@ -0,0 +1,4 @@
+package com.pcmaster.AFK.cart_shopping_management.domain.model.queries;
+
+public record GetCartShoppingByUserClientIdAndStatusQuery(Long userClientId, String statusCartShopping ) {
+}
