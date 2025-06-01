141ae026f1bf6e89efb710d5e87c303abf977a98
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-06-01 17:49:20 -0500

    feat(product-management): add in queryservices create  inventory query service implements.

---
+++ src/main/java/com/pcmaster/AFK/product_management/aplication/internal/queryservices/InventoryQueryServiceImpl.java
@@ -0,0 +1,34 @@
+package com.pcmaster.AFK.product_management.aplication.internal.queryservices;
+
+import org.springframework.stereotype.Service;
+import com.pcmaster.AFK.product_management.domain.model.aggregates.Inventory;
+import com.pcmaster.AFK.product_management.domain.model.queries.GetInventoryByIdQuery;
+import com.pcmaster.AFK.product_management.domain.model.queries.GetInventoryByUserIdQuery;
+import com.pcmaster.AFK.product_management.domain.model.valueobjects.UserTechnicalId;
+import com.pcmaster.AFK.product_management.domain.services.InventoryQueryService;
+import com.pcmaster.AFK.product_management.infrastructure.persistence.jpa.repositories.InventoryRepository;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class InventoryQueryServiceImpl implements InventoryQueryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    public InventoryQueryServiceImpl(InventoryRepository inventoryRepository) {
+        this.inventoryRepository = inventoryRepository;
+    }
+
+
+    @Override
+    public Optional<Inventory> handle(GetInventoryByIdQuery query) {
+        return this.inventoryRepository.findById(query.inventoryId());
+    }
+
+    @Override
+    public List<Inventory> handle(GetInventoryByUserIdQuery query) {
+        return this.inventoryRepository.findByUserTechnicalId(new UserTechnicalId(query.userTechnicalId()));
+    }
+}
