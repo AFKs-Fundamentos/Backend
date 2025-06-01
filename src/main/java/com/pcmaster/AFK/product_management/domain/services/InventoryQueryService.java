2f1648edf235838fdb6010b8db497ab0372a51af
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-06-01 17:43:14 -0500

    feat(product-management): add in services create  inventory query service.

---
+++ src/main/java/com/pcmaster/AFK/product_management/domain/services/InventoryQueryService.java
@@ -0,0 +1,15 @@
+package com.pcmaster.AFK.product_management.domain.services;
+
+import com.pcmaster.AFK.product_management.domain.model.aggregates.Inventory;
+import com.pcmaster.AFK.product_management.domain.model.queries.GetInventoryByIdQuery;
+import com.pcmaster.AFK.product_management.domain.model.queries.GetInventoryByUserIdQuery;
+
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface InventoryQueryService {
+    Optional<Inventory> handle(GetInventoryByIdQuery query);
+    List<Inventory> handle(GetInventoryByUserIdQuery query);
+}
