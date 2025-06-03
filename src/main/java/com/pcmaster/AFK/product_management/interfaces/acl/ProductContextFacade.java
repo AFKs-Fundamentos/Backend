60c308f62ab15659ac92431a67716aa25ab87b07
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-06-03 15:08:43 -0500

    feat(product-management): add fetch product name by id.

--- src/main/java/com/pcmaster/AFK/product_management/interfaces/acl/ProductContextFacade.java
+++ src/main/java/com/pcmaster/AFK/product_management/interfaces/acl/ProductContextFacade.java
@@ -11,5 +11,16 @@ public class ProductContextFacade {
     public ProductContextFacade(ProductQueryService productQueryService) {
         this.productQueryService = productQueryService;
     }
-
+    /**
+     * Retrieves the product name by product ID.
+     *
+     * @param productId the ID of the product whose name is to be retrieved
+     * @return an Optional containing the name of the product associated with the specified product ID, or empty if not found
+     */
+    public Optional<String> fetchProductNameById(Long productId) {
+        var getProductByIdQuery = new GetProductByIdQuery(productId);
+        var optionalProduct = productQueryService.handle(getProductByIdQuery);
+        Optional<String> productName = optionalProduct.map(product -> product.getProductName());
+        return productName;
+    }
 }
