93883578d8eeec84d799a759d23462208ec1914b
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2025-06-02 21:19:38 -0500

    feat(product-management): add acl agregate boolean  inventory exists product id .

--- src/main/java/com/pcmaster/AFK/product_management/interfaces/acl/InventoryContextFacade.java
+++ src/main/java/com/pcmaster/AFK/product_management/interfaces/acl/InventoryContextFacade.java
@@ -1,9 +1,12 @@
 package com.pcmaster.AFK.product_management.interfaces.acl;
 
 import com.pcmaster.AFK.product_management.domain.model.commands.DeleteInventoryByProductIdCommand;
+import com.pcmaster.AFK.product_management.domain.model.queries.GetInventoryByProductIdQuery;
 import com.pcmaster.AFK.product_management.domain.services.InventoryCommandService;
 import com.pcmaster.AFK.product_management.domain.services.InventoryQueryService;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class InventoryContextFacade {
@@ -16,14 +19,27 @@ public class InventoryContextFacade {
         this.inventoryQueryService = inventoryQueryService;
     }
 
+
     /**
      * Deletes an inventory by product ID.
      *
      * @param productId the ID of the product whose inventory is to be deleted
      */
+
     public void deleteInventoryByProductId(Long productId) {
        var deleteInventoryByProductIdCommand = new DeleteInventoryByProductIdCommand(productId);
        inventoryCommandService.handle(deleteInventoryByProductIdCommand);
 
+    }
+
+    /**
+     * Retrieves the inventory by product ID.
+     *
+     * @param productId the ID of the product whose inventory is to be retrieved
+     * @return the inventory associated with the specified product ID
+     */
+    public boolean inventoryExistsByProductId(Long productId) {
+        var getInventoryByProductIdQuery = new GetInventoryByProductIdQuery(productId);
+        return inventoryQueryService.handle(getInventoryByProductIdQuery).isPresent();
     }
 }
